7b07c4a39054a1818dafb0e91364ab761ea9ccc9
Engkos <engkoskostaman97@gmail.com>
2019-08-01 02:04:55 +0700

    tugas 7 js
    tugas ini kami kumpulkan untuk niomic

---
+++ tugas7.js/index.js
@@ -0,0 +1,15 @@
+//perulangan biasa
+function perulanganbiasa(){
+  for(var i=163 ;i<175;i++){
+    console.log("tinggi badan:",i)
+  }
+
+}
+
+perulanganbiasa()
+//perulangan for of
+// var angka =[163,165,168,169,170,171,172,173,174,175];
+// for(let x of angka){
+//   console.log(x);
+//
+// }
